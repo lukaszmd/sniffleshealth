8d26fae45554af09c371b5615af93620470ed256
Priyanshu Singh <spriyanshu0510@gmail.com>
2025-12-24 13:30:18 +0530

    refactor: remove unnecessary empty lines in index.ts to enhance code clarity and maintainability

--- client/features/consultation/pages/index.ts
+++ client/features/consultation/pages/index.ts
@@ -6,5 +6,3 @@ export { default as Symptoms } from "./Symptoms";
 export { default as MedicalProfile } from "./MedicalProfile";
 export { default as Consultation } from "./Consultation";
 export { default as SummaryConsultation } from "./SummaryConsultation";
-
-
